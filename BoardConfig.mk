8b824147842dc2c08ac1f964aaa593527ea5b9ec
cphelps76 <cphelps76@gmail.com>
2013-08-09 22:13:13 -0500

    DC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,8 +22,10 @@
 #TARGET_KERNEL_SOURCE := kernel/samsung/n8000
 #TARGET_KERNEL_CONFIG := cyanogenmod_n8013_defconfig
 
-TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := cyanogenmod_n8013_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/n8013
+TARGET_KERNEL_CONFIG := DEMENTEDConcepts_n8013_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+TARGET_GCC_VERSION := 4.7
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := c0,p4notewifi,p4notewifiww,n8013,GT-N8013
